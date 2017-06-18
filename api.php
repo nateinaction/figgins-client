a9c47cc2055b4d5bc0200e76531a1e139c7c2b4b
Nate Gay <nate.gay@wpengine.com>
2017-06-18 16:15:49 -0500

    JWT verification working

---
+++ api.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Rest endpoint class
+ * Run, Check, Fix
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+class Figgins_Client_API {
+  public function register_routes() {
+    $version = '1';
+    $namespace = 'figgins_client/v' . $version;
+
+    /*
+     * Register POST to run a break
+     * /wp-json/figgins_client/v1/poke/#
+     */
+    register_rest_route( $namespace, '/poke', array(
+      'methods' => WP_REST_Server::CREATABLE,
+      'callback' => array( $this, 'mirror_token' ),
+      'permission_callback' => array( $this, 'verify_token' ),
+    ));
+
+  }
+
+  public function mirror_token( $request ) {
+    return $request['token'];
+  }
+
+  public function verify_token( $request ) {
+    $token = (new Parser())->parse((string) $request['token']);
+    $secret = 'poke123';
+    //$secret = get_option( 'figgins_client_secret' );
+    $signer = new Sha256();
+
+    // if token does not match or has not been defined
+    return $token->verify($signer, $secret);
+  }
+}
+
+/**
+* Register routes class
+*/
+function register_figgins_client_api() {
+   $controller = new Figgins_Client_API();
+   $controller->register_routes();
+}
+
+add_action( 'rest_api_init', 'register_figgins_client_api' );
---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Figgins Client
+Version:     0.0.1
+Author:      Nate Gay
+Author URI:  https://worldpeace.io/
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+*/
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/endpoint.php';
